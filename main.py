6452c45efc0fdaca1325fe5af88221e9392078c7
kenny11cz <dan.balarin@gmail.com>
2015-01-07 18:37:55 +0100

    Zkouska GPIO 3

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
+print "Verze GPIO knihovny"
+print GPIO.VERSION
 
-GPIO.VERSION
-print "test"
+GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
+GPIO.output(7,True) ## Turn on GPIO pin 7
+print "Zapnuto"
